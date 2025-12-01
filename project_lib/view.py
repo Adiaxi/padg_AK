d613358a576ab32a4c0848d8fba6d7ce040f8fe4
Adiaxi <u56129@student.wat.edu.pl>
2025-12-01 23:36:15 +0100

    feat: Login GUI button functionality added

--- project_lib/view.py
+++ project_lib/view.py
@@ -13,28 +13,30 @@ class Login:
         self.root.geometry("400x400")
 
         self.log_frame = Frame(self.root)
+        self.root.grid_rowconfigure(0, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
         self.log_frame.grid(column=0, row=0)
 
         self.build_login()
-    #     self.log(str(input("Login: ")),str(input("Password: ")))
-    #
-    # def log(self,log:str,psswd:str):
-    #     if  psswd == 'admin' and log == 'admin':
-    #         app=AppView()
-    #         app.run()
-    #         self.run()
+
+    def log(self,log:str,psswd:str):
+        if  psswd == 'admin' and log == 'admin':
+            app=AppView()
+            app.run()
+            self.run()
 
     def build_login(self):
-        self.log_name=Label(self.log_frame,text="Login")
-        self.log_psswd=Label(self.log_frame,text="Password")
+        self.log_name=Label(self.log_frame,text="Login", font=("Segoe UI", 14, 'bold'))
+        self.log_psswd=Label(self.log_frame,text="Password",font=("Segoe UI", 14, 'bold'))
         self.entry_name=Entry(self.log_frame)
         self.entry_psswd=Entry(self.log_frame)
 
-        self.log_name.grid(column=0,row=0,padx=5,pady=5)
-        self.log_psswd.grid(column=1,row=0,padx=5,pady=5)
-        self.entry_name.grid(column=0,row=1)
-        self.entry_psswd.grid(column=1,row=1)
-        self.log_psswd.focus()
+        self.log_name.grid(column=0,row=0,padx=10,pady=10,sticky='W')
+        self.log_psswd.grid(column=1,row=0,padx=10,pady=10,sticky="W")
+        self.entry_name.grid(column=0,row=1,padx=10,pady=10)
+        self.entry_psswd.grid(column=1,row=1,padx=10,pady=10)
+        self.log_button=Button(self.log_frame,text='Login', command=lambda: self.log(self.entry_name.get(),self.entry_psswd.get()))
+        self.log_button.grid(column=0,row=2,padx=10,pady=10,sticky='W')
 
     def run(self):
          self.root.mainloop()
